c202623924ab743f8be451b69c1162bfe016cc96
Atharizaldi <ketel.pans@gmail.com>
2023-08-26 07:39:07 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/praktik_mochammad_athar_rizaldi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.praktik_mochammad_athar_rizaldi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:praktik_mochammad_athar_rizaldi/pages/entry_data.dart';
+import 'package:praktik_mochammad_athar_rizaldi/pages/show_data.dart';
+
+import 'pages/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/form_entry': (context) => EntryData(),
+        '/show_data': (context) => ShowData()
+      },
+    );
+  }
+}
